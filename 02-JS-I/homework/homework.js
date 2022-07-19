77590f917b5503ff53a313edbbff647f5606d770
Valentín Ceballos <ing.javier.ceballos@gmail.com>
2022-07-18 21:55:06 -0500

    02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -263,9 +263,19 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+  {
+    return 'Es Vocal';
+  }
+  else if (letra.length > 1)
+  {
+    return 'Dato Incorrecto';
+  }
+  else
+  {
+    return 'Dato Incorrecto';
+  }
 }
-
 
 
 // No modificar nada debajo de esta línea
