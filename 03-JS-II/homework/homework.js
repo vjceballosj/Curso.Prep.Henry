ef0acc63258f5d499e028b61bd899daca83366e8
vjceballosj <ing.javier.ceballos@gmail.com>
2022-08-02 21:39:05 -0500

    homework_03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -112,12 +112,28 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero===5 || numero===10)
+  {
+    return true;
+  }
+    else
+    {
+      return false;
+    }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero>20 && numero<50)
+  {
+    return true;
+  }
+    else
+    {
+      return false;
+    }
 }
 
 function esEntero(numero) {
@@ -128,6 +144,14 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if(numero%1===0)
+  {
+    return true;
+  }
+    else
+    {
+      return false;
+    }
 }
 
 function fizzBuzz(numero) {
@@ -135,6 +159,22 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero % 3 === 0 && numero % 5 ===0)
+  {
+    return "fizzbuzz";
+  }
+    else if(numero % 3 === 0)
+    {
+      return "fizz";
+    }
+      else if(numero % 5 === 0) 
+      {
+        return "buzz";
+      }
+        else
+        {
+          return numero;
+        }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -144,6 +184,26 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if(num1 < 0 || num2 < 0 || num3 < 0)
+  {
+    return "Hay negativos";
+  }
+    else if (num1 === 0 || num2 === 0 || num3 === 0)
+    {
+      return "Error";
+    }
+      else if (num1 > num2 && num1 > num3 && num1 > 0)
+      {
+        return "Número 1 es mayor y positivo";
+      }
+        else if (num3 > num1 && num3 > num2)
+        {
+          return num3 = num3 + 1;
+        }
+          else
+            {
+              return false;
+            }
 }
 
 function esPrimo(numero) {
